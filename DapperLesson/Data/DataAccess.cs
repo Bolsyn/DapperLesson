fd5fc104fa7ebdf5d8b3fea846409a39b36cd8e4
Bolsyn <Serikagis@yandex.ru>
2021-03-17 10:02:19 +0600

    All In One

--- DapperLesson/Data/DataAccess.cs
+++ DapperLesson/Data/DataAccess.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace DapperLesson.Data
 {
-    public abstract class DataAccess<T> : IDisposable
+    public class DataAccess<T> : IDisposable
     {
         protected readonly DbProviderFactory factory;
         protected readonly DbConnection connection;
@@ -25,13 +28,58 @@ namespace DapperLesson.Data
             connection.Close();
         }
 
-        public abstract void Insert(T entity);
+        public void Insert(T entity)
+        {
+            using (var connection = new SqlConnection())
+            {
+                if (entity.Equals("FullName")) 
+                { 
+                    connection.Execute($"Insert into Players values (@Id, @FullName, @Number)", entity);
+                }
+                if (entity.Equals("Name"))
+                {
+                    connection.Execute($"Insert into Teams values (@Id, @Name)", entity);
+                }
+            }
+        }
 
-        public abstract void Update(T entity);
+        public void Update(T entity)
+        {
+            using (var connection = new SqlConnection())
+            {
+                if (entity.Equals("FullName"))
+                {
+                    connection.Execute("Update Players SET FullName = @FullName Set Number= @Number Where Id = @Id;", entity);
+                }
+                if (entity.Equals("Name"))
+                {
+                    connection.Execute("Update Teams SET Name = @Name Where Id = @Id;", entity);
+                }
+            }
+        }
 
-        public abstract void Delete(T entity);
+        public void Delete(T entity)
+        {
+            using (var connection = new SqlConnection())
+            {
+                if (entity.Equals("FullName"))
+                {
+                    connection.Execute("Delete From Players Where Id = @Id", entity);
+                }
+                if (entity.Equals("Name"))
+                {
+                    connection.Execute("Delete From Teams Where Id = @Id", entity);
+                }
+            }
+        }
 
-        public abstract ICollection<T> GetAll();
+        public ICollection<T> GetAll()
+        {
+            using (var connection = new SqlConnection())
+            {
+                return connection.Query<T>("Select * from Teams;Select *from Players").ToList();
+            }
+        }
 
         public void ExecuteTranaction(params DbCommand[] commands)
         {
